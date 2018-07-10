0fb3ee71ce2250aad84b1531923941739385913b
Han Chiang <yaphc@hotmail.com>
2018-07-10 21:27:17 +0800

    Start CrimeActivity From CrimeListFragment to show details of a crime when it is clicked. Use Fragment arguments to pass data from CrimeActiity to CrimeFragment to preserve fragment encapsulation

--- app/src/main/java/com/example/han/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/han/criminalintent/CrimeLab.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+// Singleton
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
     private List<Crime> mCrimes;
